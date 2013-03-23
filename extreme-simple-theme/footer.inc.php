c60638687828355bc215173002a3615a1572c8d4
Oliver Kamer <ok@oliver.kamer.ch>
2013-03-23 16:08:57 +0100

    renamed folder and various imporvements

---
+++ extreme-simple-theme/footer.inc.php
@@ -0,0 +1,12 @@
+<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }
+/****************************************************
+*
+* @File:      footer.inc.php
+* @Package:   GetSimple
+* @Action:    Simple Theme for GetSimple CMS
+*
+*****************************************************/
+?>
+<p>
+    &copy; by <?php getPageField(return_page_slug(),"author"); ?> <?php get_page_date("Y"); ?> - <?php echo date("Y"); ?>
+</p>
