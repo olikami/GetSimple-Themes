8fd9b027ae7212a7bf35295f04d4189ed7c234f9
Oliver Kamer <ok@oliver.kamer.ch>
2013-03-24 14:54:48 +0100

    deleted stupid stuff

--- extreme-simple-theme/assets/header.inc.php
+++ extreme-simple-theme/assets/header.inc.php
@@ -1,17 +1,16 @@
-<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }
-/****************************************************
-*
-* @File:      header.inc.php
-* @Package:   GetSimple
-* @Action:    Simple Theme for GetSimple CMS
-*
-*****************************************************/
-?>
-<head>
-    <title><?php get_page_clean_title(); ?> - <?php get_site_name(); ?></title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<link rel="stylesheet" href="<?php get_theme_url(); ?>/css/style.css">
-    <?php get_header(); ?>
+<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }
+/****************************************************
+*
+* @File:      header.inc.php
+* @Package:   GetSimple
+* @Action:    Simple Theme for GetSimple CMS
+*
+*****************************************************/
+?>
+<head>
+    <title><?php get_page_clean_title(); ?> - <?php get_site_name(); ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+	<link rel="stylesheet" href="<?php get_theme_url(); ?>/css/style.css">
+    <?php get_header(); ?>
 </head>
--- extreme-simple-theme/description.md
+++ extreme-simple-theme/description.md
@@ -1,8 +1,4 @@
-Extreme-Simple-Theme (1.1.1)
+Extreme-Simple-Theme (1.1.0)
 ==
 
-A simple but nice theme for the GetSimple CMS. There is no Javascript, so it shout work on many Browser.
-
-<a href="http://www.w3.org/html/logo/">
-<img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3.png" width="133" height="64" alt="HTML5 Powered with CSS3 / Styling" title="HTML5 Powered with CSS3 / Styling">
-</a>
+A simple but nice theme, no javascript.
